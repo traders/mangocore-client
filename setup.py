96cb4d0d55bf259cedcf443f0eaa33abc77f2d85
Phil <philwsun@gmail.com>
2016-10-03 00:17:13 -0400

    Remove dist folder, move tradersbot.py to __init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
 	name = 'tradersbot',
 	packages = ['tradersbot'], # this must be the same as the name above
-	version = '0.1',
+	version = '0.1.2',
 	description = 'Python wrapper for mangocore API',
 	author = 'Traders@MIT',
 	author_email = 'traders@mit.edu',
 	url = 'https://github.com/traders/mangocore-client',
-	download_url = 'https://github.com/traders/mangocore-client/tarball/0.1',
+	download_url = 'https://github.com/traders/mangocore-client/tarball/0.1.2',
+	install_requires = ['tornado'],
 	keywords = ['MIT', 'quant', 'traders', 'trading'],
 	classifiers = [],
 )
